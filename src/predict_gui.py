696fa9703b264c220a426459d6d2f671699c6848
yangmh <fastyangmh@gmail.com>
2022-02-11 16:05:21 +0800

    update config yaml. rewrite predict.py to support gradio API. support web interface in predict gui.

--- src/predict_gui.py
+++ src/predict_gui.py
@@ -9,6 +9,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from playsound import playsound
 import tkinter as tk
+import gradio as gr
 
 
 # class
@@ -21,6 +22,9 @@ class PredictGUI(BasePredictGUI):
         self.transform = parse_transforms(
             transforms_config=project_parameters.transforms_config)['predict']
         self.sample_rate = project_parameters.sample_rate
+        self.web_interface = project_parameters.web_interface
+        self.examples = project_parameters.examples if len(
+            project_parameters.examples) else None
 
         # button
         self.play_button = Button(master=self.window,
@@ -74,7 +78,7 @@ class PredictGUI(BasePredictGUI):
 
     def recognize(self):
         if self.filepath is not None:
-            predicted = self.predictor.predict(filepath=self.filepath)
+            predicted = self.predictor.predict(inputs=self.filepath)
             text = ''
             for idx, (c, p) in enumerate(zip(self.classes, predicted)):
                 text += '{}: {}, '.format(c, p.round(3))
@@ -93,20 +97,34 @@ class PredictGUI(BasePredictGUI):
         else:
             messagebox.showerror(title='Error!', message='please open a file!')
 
+    def inference(self, inputs):
+        prediction = self.predictor.predict(inputs=inputs)
+        result = {c: p for c, p in zip(self.classes, prediction)}
+        return result
+
     def run(self):
-        # NW
-        self.open_file_button.pack(anchor=tk.NW)
-        self.recognize_button.pack(anchor=tk.NW)
-        self.play_button.pack(anchor=tk.NW)
+        if self.web_interface:
+            gr.Interface(fn=self.inference,
+                         inputs=gr.inputs.Audio(source='microphone',
+                                                type='filepath'),
+                         outputs='label',
+                         examples=self.examples,
+                         interpretation="default").launch(share=True,
+                                                          inbrowser=True)
+        else:
+            # NW
+            self.open_file_button.pack(anchor=tk.NW)
+            self.recognize_button.pack(anchor=tk.NW)
+            self.play_button.pack(anchor=tk.NW)
 
-        # N
-        self.filepath_label.pack(anchor=tk.N)
-        self.image_canvas.get_tk_widget().pack(anchor=tk.N)
-        self.predicted_label.pack(anchor=tk.N)
-        self.result_label.pack(anchor=tk.N)
+            # N
+            self.filepath_label.pack(anchor=tk.N)
+            self.image_canvas.get_tk_widget().pack(anchor=tk.N)
+            self.predicted_label.pack(anchor=tk.N)
+            self.result_label.pack(anchor=tk.N)
 
-        # run
-        super().run()
+            # run
+            super().run()
 
 
 if __name__ == '__main__':
